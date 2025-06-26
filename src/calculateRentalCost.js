3eced8af28085b221a83057e5ac6f753ab505daa
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-06-26 20:52:31 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,23 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const costPerDay = 40;
+  const middleTermStart = 3;
+  const middleTermFinish = 6;
+  const basicDiscount = 20;
+  const longTerm = 7;
+  const additionalDiscount = 50;
+  const baseRentalCost = costPerDay * days;
+
+  if (days >= middleTermStart && days <= middleTermFinish) {
+    return baseRentalCost - basicDiscount;
+  }
+
+  if (days >= longTerm) {
+    return baseRentalCost - additionalDiscount;
+  }
+
+  return baseRentalCost;
 }
 
 module.exports = calculateRentalCost;
